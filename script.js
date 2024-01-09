bc0df05b26c667bac11f168fe8d346d65a14aee9
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-09 15:14:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript 
+function displaySelectedPackage() {
+    var selectedPackage = document.getElementById("package-select").value;
+    document.getElementById("selected-package").innerText = selectedPackage;
+}
