73688dd21d1048916719f7083abd2b4cb6a522bb
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-18 22:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function displaySelectedPackage() {
     var selectedPackage = document.getElementById("package-select").value;
     document.getElementById("selected-package").innerText = selectedPackage;
 }
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000, // Adjust the speed as needed
+        dots: true, // Show navigation dots
+        arrows: true // Show navigation arrows
+        // Add more configuration options based on your requirements
+    });
+});
