de46f845d1b4650f88466ca763230e09f74fb38a
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-19 23:02:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel').slick({
         autoplay: true,
-        autoplaySpeed: 2000, // Adjust the speed as needed
+        autoplaySpeed: 3000, // Adjust the speed as needed
         dots: true, // Show navigation dots
         arrows: true // Show navigation arrows
         // Add more configuration options based on your requirements
