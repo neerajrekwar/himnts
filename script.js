b0dccb9bba7fae13ab8499b19b40db4692da795e
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-29 12:37:58 +0530

    gfds

--- script.js
+++ script.js
@@ -11,5 +11,34 @@ buttonThree.addEventListener('click', () => {
     }
 })
 
+  $(document).ready(function(){
+    // Initialize each carousel
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      arrows: false,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+  });
+
+
 
 
