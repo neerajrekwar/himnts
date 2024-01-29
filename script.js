e7beca1047236b475afdc8d48edcf411a59afb2d
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-29 21:06:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,21 +11,3 @@ buttonThree.addEventListener('click', () => {
     }
 })
 
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:3
-        },
-        1000:{
-          items:5
-        }
-      }
-    });
-  });
