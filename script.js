76a017fd933074edae86d701f82fedc93f32098f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-01-29 16:36:20 +0530

    fdas

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ buttonThree.addEventListener('click', () => {
         buttonThree.setAttribute('aria-expanded', 'false');
     }
 })
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:3
+        },
+        1000:{
+          items:5
+        }
+      }
+    });
+  });
