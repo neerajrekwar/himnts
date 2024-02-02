3f749f3f0a5c636df281a74f67eed2b7fc8f068c
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-02 22:49:38 +0530

    gfds

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,3 @@ myNavee.addEventListener("click", () => {
     myNavee.setAttribute("aria-expanded", "false");
   }
 })
-
-document.getElementById("myHead").innerHTML =
-	"<span id='headerText'>Title</span>"
-	+ "<span id='headerSubtext'>Subtitle</span>";
-document.getElementById("myNav").innerHTML =
-	"<ul id='navLinks'>"
-	+ "<li><a href='index.html'>Home</a></li>"
-	+ "<li><a href='about.html'>About</a>"
-	+ "<li><a href='donate.html'>Donate</a></li>"
-	+ "</ul>";
-document.getElementById("myFooter").innerHTML =
-	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
-	+ " rights reserved.</p>"
-	+ "<p id='credits'>Layout by You</p>"
-	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
-	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
