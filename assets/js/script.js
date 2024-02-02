43970918988075c7015650a3980b3a907affa70c
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-02 17:24:33 +0530

    gs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 const myNavee = document.querySelector(".button-three");
 
-myNavee.addEventListener("click", () => {
-  const menu = document.querySelector(".navbar");
-  menu.classList.toggle("active");
-  const isOpened = myNavee.getAttribute("aria-expanded");
-  if (isOpened === "false") {
-    myNavee.setAttribute("aria-expanded", "true");
-  } else {
-    myNavee.setAttribute("aria-expanded", "false");
-  }
-})
+if (myNavee) {
+  myNavee.addEventListener("click", () => {
+    const menu = document.querySelector(".navbar");
+
+    if (menu) {
+      menu.classList.toggle("active");
+
+      const isOpened = myNavee.getAttribute("aria-expanded");
+
+      if (isOpened === "false") {
+        myNavee.setAttribute("aria-expanded", "true");
+      } else {
+        myNavee.setAttribute("aria-expanded", "false");
+      }
+    }
+  });
+}
