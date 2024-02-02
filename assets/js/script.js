a0087f421ff7ba63a5d4ae68aecc3863f1d92ed0
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-02 17:28:15 +0530

    gfs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,12 @@
 const myNavee = document.querySelector(".button-three");
 
-if (myNavee) {
-  myNavee.addEventListener("click", () => {
-    const menu = document.querySelector(".navbar");
-
-    if (menu) {
-      menu.classList.toggle("active");
-
-      const isOpened = myNavee.getAttribute("aria-expanded");
-
-      if (isOpened === "false") {
-        myNavee.setAttribute("aria-expanded", "true");
-      } else {
-        myNavee.setAttribute("aria-expanded", "false");
-      }
-    }
-  });
-}
+myNavee.addEventListener("click", () => {
+  const menu = document.querySelector(".navbar");
+  menu.classList.toggle("active");
+  const isOpened = myNavee.getAttribute("aria-expanded");
+  if (isOpened === "false") {
+    myNavee.setAttribute("aria-expanded", "true");
+  } else {
+    myNavee.setAttribute("aria-expanded", "false");
+  }
+})
