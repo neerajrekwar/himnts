c5223fdd6e9ccb6d502e7d3d3a4cd02583b9efba
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-04 18:43:51 +0530

    gfsd

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,14 +11,3 @@ myNavee.addEventListener("click", () => {
   }
 });
 
-$(document).ready(function(){
-  $('.carousel').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000 // Set the speed in milliseconds
-  });
-});
