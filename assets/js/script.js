eed2c63c229166e524aa62f4fa01ea60498f7ca8
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-10 21:53:28 +0530

    fda

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,9 @@ myNavee.addEventListener("click", () => {
   }
 });
 
- // Get the select element
- const prizeSelect = document.getElementById('prizeSelect');
-    
- // Get the total prize span element
- const totalPrizeSpan = document.getElementById('totalPrize');
+ let addOn = document.getElementById("prizeSelect").value;
+ let newPrice = document.getElementById("totalPrize");
 
- // Add an event listener to the select element
- prizeSelect.addEventListener('change', updateTotalPrize);
+ const tatal = addOn + newPrice;
 
- // Function to update the total prize based on the selected prize
- function updateTotalPrize() {
-   // Get the selected prize value
-   const selectedPrize = parseFloat(prizeSelect.value);
-
-   // Update the total prize
-   totalPrizeSpan.textContent = selectedPrize.toFixed(2);
- }
+ document.getElementById("totalPrice").textContent = total.toFixed(2);
