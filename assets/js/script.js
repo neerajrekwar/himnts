1ebeecffdff144b595015a01a2812cc89496f462
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-02-15 16:30:14 +0530

    h

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,4 @@ myNavee.addEventListener("click", () => {
   }
 })
 
- function sendWhatsAppMessages() {
-  
-  const packages = [
-    { name: 'Joyful', price: '&#8377;1399' },
-    { name: 'Delight', price: '&#8377;2199' },
-    { name: 'Extravaganza', price: '&#8377;3499' },
-    // Add more packages as needed
-  ];
-  
-
-    packages.forEach(package  =>  {
-      const message = `Hello! I would like to book the following package:\n\nName: ${package.name}\nPrice: ${package.price}`;
-
-      // Replace the following number with the desired WhatsApp number
-      const phoneNumber = '+918882358053';
-
-      const waMeLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-
-      // Open WhatsApp link in a new tab for each package
-      window.open(waMeLink, '_blank');
-    });
-  }
-
 // me.mindStet(life.change('none').value);
