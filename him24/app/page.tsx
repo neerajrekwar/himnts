0cae71d2a6d021f4c5b277b6a563bc13ab6caf36
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-05-09 07:34:42 +0000

    jeddgfdg

---
+++ him24/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main>
+      <section className="m-2">
+        <h1 className="text-2xl font-bold">hello, my nextjs app</h1>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+          adipisci nostrum dicta reiciendis maiores doloremque, culpa unde
+          tempora assumenda modi, molestiae ipsum. Libero quasi ducimus tempora
+          neque voluptas architecto! Odit?
+        </p>
+      </section>
+      <section className="m-2">
+        <h1 className="text-2xl font-bold">hello, my nextjs app</h1>
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+          adipisci nostrum dicta reiciendis maiores doloremque, culpa unde
+          tempora assumenda modi, molestiae ipsum. Libero quasi ducimus tempora
+          neque voluptas architecto! Odit?
+        </p>
+      </section>
+    </main>
+  );
+}
