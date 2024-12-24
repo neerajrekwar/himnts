b5b5794f74e9249f3ec07c5a54b8c33e601f6e4a
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-24 14:32:47 +0000

    fdsa

---
+++ demo-app/src/app/products/page.tsx
@@ -0,0 +1,9 @@
+import Products from "@/components/Products";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen  flex-col items-center justify-center p-24">
+      <Products/>
+    </main>
+  );
+}
