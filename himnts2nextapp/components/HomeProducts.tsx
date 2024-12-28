ca5c41a38229e853e56e137c5f2c42f0f766192d
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-28 10:17:16 +0000

    gfd

---
+++ himnts2nextapp/components/HomeProducts.tsx
@@ -0,0 +1,96 @@
+'use client'
+import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
+
+export function HomeProducts() {
+  return (
+
+      <div className="m-auto  max-w-6xl gap-2 grid grid-cols-12 grid-rows-2 py-8 px-8">
+        <Card className="col-span-12 sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
+            <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1504196606672-aef5c9cefc92?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card className="col-span-12 sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
+            <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1555276831-f5996bfbb1cc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card className="col-span-12 sm:col-span-4 h-[300px]">
+          <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Supercharged</p>
+            <h4 className="text-white font-medium text-large">Creates beauty like a beast</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1521472386493-d57e3bfc3ef3?q=80&w=1441&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+        </Card>
+        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-5">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">New</p>
+            <h4 className="text-black font-medium text-2xl">Acme camera</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Card example background"
+            className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
+            src="https://images.unsplash.com/photo-1602631985686-1bb0e6a8696e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
+            <div>
+              <p className="text-black text-tiny">Available soon.</p>
+              <p className="text-black text-tiny">Get notified.</p>
+            </div>
+            <Button className="text-tiny" color="primary" radius="full" size="sm">
+              Notify Me
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
+          <CardHeader className="absolute z-10 top-1 flex-col items-start">
+            <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+            <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
+          </CardHeader>
+          <Image
+            removeWrapper
+            alt="Relaxing app background"
+            className="z-0 w-full h-full object-cover"
+            src="https://images.unsplash.com/photo-1607977166054-ba266c446ba3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+            <div className="flex flex-grow gap-2 items-center">
+              <Image
+                alt="Breathing app icon"
+                className="rounded-full w-10 h-11 bg-black"
+                src="https://nextui.org/images/breathing-app-icon.jpeg"
+              />
+              <div className="flex flex-col">
+                <p className="text-tiny text-white/60">Breathing App</p>
+                <p className="text-tiny text-white/60">Get a good night&#39;s sleep.</p>
+              </div>
+            </div>
+            <Button radius="full" size="sm">
+              Get App
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    
+  );
+}
