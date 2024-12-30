6423b000e712ad26dda4ac6a36fba5c67b0da1a3
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2024-12-30 20:34:24 +0000

    fda

--- himnts2nextapp/app/packages/page.tsx
+++ himnts2nextapp/app/packages/page.tsx
@@ -3,9 +3,7 @@ import packages from "@/public/packages/package.json"
 export default function Packages(){
     return(
         <section>
-           {packages.map((package)) => (
-            <div key={pack}></div>
-           ) : ()}
+           
         </section>
     )
 }
