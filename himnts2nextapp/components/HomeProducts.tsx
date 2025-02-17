faabfc2d0b1537609cb3904dea94b9c7e2a813a3
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2025-02-18 01:31:42 +0530

    Update HomeProducts.tsx

--- himnts2nextapp/components/HomeProducts.tsx
+++ himnts2nextapp/components/HomeProducts.tsx
@@ -3,7 +3,7 @@ import { Card, CardHeader, CardFooter, Image, Button } from "@nextui-org/react";
 
 export function HomeProducts() {
   return (
-      <div className="m-auto  max-w-6xl gap-4 grid grid-cols-12 grid-rows-2 py-8">
+      <div className="m-auto  max-w-xl gap-4 grid grid-cols-12 grid-rows-2 py-8">
         <Card className="col-span-12 sm:col-span-4 h-[300px]">
           <CardHeader className="absolute z-10 top-1 flex-col !items-start ">
             <p className="text-tiny text-white/60 uppercase font-bold">What to watch</p>
